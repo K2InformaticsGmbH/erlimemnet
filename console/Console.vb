1c36d9cfefe0f2063c3c0fa37e9ff826fadebb53
Bikram Chatterjee <razorpeak@gmail.com>
2013-04-17 13:51:33 +0200

    refactored erlnet for imem with vb console app usage example

---
+++ console/Console.vb
@@ -0,0 +1,13 @@
+﻿Module Console
+
+    Sub Main()
+        Dim mpro As Mpro = mpro.Connect("localhost", 8125)
+        mpro.Authenticate("admin", "change_on_install")
+        mpro.Login()
+        Dim l As ArrayList = mpro.listDestinationChannels("smpp")
+
+        l = Nothing
+        mpro = Nothing
+    End Sub
+
+End Module
---
+++ console/Mpro.vb
@@ -0,0 +1,52 @@
+﻿Imports System.Net.Sockets
+Imports K2Informatics.Erlnet
+
+Public Class Mpro
+    Inherits Imem
+
+    Protected Sub New(ByRef _stream As NetworkStream)
+        MyBase.New(_stream)
+    End Sub
+
+    Public Overloads Shared Function Connect(ByVal host As String, ByVal port As Integer) As Mpro
+        Dim client As TcpClient = New TcpClient(host, port)
+        Dim s As NetworkStream = client.GetStream()
+        Return New Mpro(s)
+    End Function
+
+    Public Function listDestinationChannels(ByVal AppId) As ArrayList
+        Return CallMproMFASync("listDestinationChannels", AppId)
+    End Function
+    Public Function putSourcePeerName(ByRef AppId As String, ByRef Key As String, ByRef SpName As String) As ArrayList
+        Return CallMproMFASync("putSourcePeerName", AppId, Key, SpName)
+    End Function
+    Public Function deleteSourcePeer(ByRef AppId As String, ByRef Key As String) As ArrayList
+
+        Return CallMproMFASync("deleteSourcePeer", AppId, Key)
+    End Function
+    Public Function listSourcePeerKeys(ByRef AppId As String) As ArrayList
+        Return CallMproMFASync("listSourcePeerKeys", AppId)
+    End Function
+    Public Function listSourcePeers(ByRef AppId As String) As ArrayList
+        Return CallMproMFASync("listSourcePeers", AppId)
+    End Function
+    Public Function putSourcePeer(ByRef AppId As String, ByRef Key As String, ByRef ChStr As String, ByRef OptStr As String) As ArrayList
+        Return CallMproMFASync("deleteWhitelist", AppId, Key, ChStr, OptStr)
+    End Function
+    Public Function putWhitelist(ByRef AppId As String, ByRef Key As String, ByRef IpAddressStr As String, ByRef OptStr As String) As ArrayList
+        Return CallMproMFASync("deleteWhitelist", AppId, Key, IpAddressStr, OptStr)
+    End Function
+    Public Function deleteWhitelist(ByRef AppId As String, ByRef Key As String, ByRef IpAddressStr As String) As ArrayList
+        Return CallMproMFASync("deleteWhitelist", AppId, Key, IpAddressStr)
+    End Function
+    Public Function getSourcePeer(ByRef AppId As String, ByRef Key As String) As ArrayList
+        Return CallMproMFASync("getSourcePeer", AppId, Key)
+    End Function
+    Public Function getWhitelist(ByRef AppId As String, ByRef Key As String) As ArrayList
+        Return CallMproMFASync("getWhitelist", AppId, Key)
+    End Function
+    Private Function CallMproMFASync(ByVal fun As String, ByVal ParamArray argsRest() As Object) As ArrayList
+        Return CallImemMFASync("mpro_dal_prov", fun, argsRest)
+    End Function
+
+End Class
