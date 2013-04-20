aedb6dba15d36021c3cb25a61eb560cea518211e
Stefan Ochsenbein <ochsenbein@bluewin.ch>
2013-04-20 13:42:06 +0200

    recompile with binary string transfer and add test case for long strings > 65KB

--- console/Console.vb
+++ console/Console.vb
@@ -1,4 +1,6 @@
-﻿Module Console
+﻿Imports System.Text
+
+Module Console
 
     Sub Main()
         Dim mpro As Mpro = mpro.Connect("localhost", 8125)
@@ -7,8 +9,11 @@
         ' mpro.Logout()
         ' mpro.Authenticate("admin", "change_on_install")
         ' mpro.Login()
-
-        Dim l As ArrayList = mpro.putSourcePeer("smpp", "1234", "3017", "Schüpbach", "[]")
+        Dim strTest As New StringBuilder
+        For i = 1 To 10000
+            strTest.Append("Schüpbach")
+        Next
+        Dim l As ArrayList = mpro.putSourcePeer("smpp", "1234", "3017", strTest.ToString, "[]")
         l = Nothing
         mpro = Nothing
     End Sub
